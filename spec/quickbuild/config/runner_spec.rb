88e97253feb814d81d4643e54d4195c54b411ac9
Alexey Shumkin <Alex.Crezoff@gmail.com>
2017-08-23 18:44:43 +0300

    fixed: test: runner_spec: we does not test requests actually
    As we've mocked request factory we do not test request factory.

--- spec/quickbuild/config/runner_spec.rb
+++ spec/quickbuild/config/runner_spec.rb
@@ -20,13 +20,8 @@ RSpec.describe Runner do
     context 'run build' do
       FAKE_CONFIG_ID = '100'
 
-      let(:runner_run_build_root_example1) do
+      let(:runner_run_build_root_example) do
         cliOptions = RunnerOptions.new ['--run-build', 'root/example']
-        Runner.new(cliOptions)
-      end
-
-      let(:runner_run_build_root_example2) do
-        cliOptions = RunnerOptions.new ['--run-build', 'root/example2']
         Runner.new(cliOptions)
       end
 
@@ -35,7 +30,7 @@ RSpec.describe Runner do
       end
 
       let(:mock_request_factory) do
-        double('mock_request_factory')
+        Request::Factory.new
       end
 
       let(:mock_get_configuration_id_response) do
@@ -43,22 +38,6 @@ RSpec.describe Runner do
         allow(mock_response).to receive(:ok?).and_return(true)
         allow(mock_response).to receive(:body).and_return(FAKE_CONFIG_ID)
         mock_response
-      end
-
-      let(:request_get_configuration_id) do
-        Request::GetConfigurationID.new('SERVER', 'root/example')
-      end
-
-      it "runs build: root/example1" do
-        mock_request_handler = double('root/example')
-
-        allow(mock_request_handler).to receive(:execute_request).and_return(mock_get_configuration_id_response)
-
-        allow(mock_request_factory).to receive(:create).and_return(request_get_configuration_id)
-
-        mock_action_factory = Action::Factory.new(mock_request_handler, mock_request_factory, mock_credentials)
-
-        expect(runner_run_build_root_example1.run(mock_action_factory)).to eql(FAKE_CONFIG_ID)
       end
 
       let(:mock_run_build_response) do
@@ -76,20 +55,13 @@ RESPONSE
         mock_response
       end
 
-      let(:request_run_build) do
-        Request::RunBuild.new('SERVER', 100, VariableList.new)
-      end
-
-      it "runs another build: root/example2" do
-        allow(mock_request_factory).to receive(:create).with(:get_configuration_id, any_args).and_return(request_get_configuration_id)
-        allow(mock_request_factory).to receive(:create).with(:run_build, any_args).and_return(request_run_build)
-
-        mock_request_handler = double('root/example2')
+      it "runs a build: root/example" do
+        mock_request_handler = double('root/example')
         allow(mock_request_handler).to receive(:execute_request).with(instance_of(Request::GetConfigurationID), any_args).and_return(mock_get_configuration_id_response)
         allow(mock_request_handler).to receive(:execute_request).with(instance_of(Request::RunBuild), any_args).and_return(mock_run_build_response)
 
         mock_action_factory = Action::Factory.new(mock_request_handler, mock_request_factory, mock_credentials)
-        expect(runner_run_build_root_example2.run(mock_action_factory)).to eql('build request GUID')
+        expect(runner_run_build_root_example.run(mock_action_factory)).to eql('build request GUID')
       end
 
     end
